9df8d953aae970d2d699cab209ce27121173dd72
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2024-06-11 15:31:59 -0500

    implement algolia search bar 100%

--- src/app/components/AlgoliaSearchComponent.tsx
+++ src/app/components/AlgoliaSearchComponent.tsx
@@ -1,9 +1,13 @@
-"use client";
-
-import React, { useEffect, useState, MouseEvent, ChangeEvent } from "react";
+import React, {
+  useRef,
+  useEffect,
+  useState,
+  MouseEvent,
+  ChangeEvent,
+} from "react";
 import algoliasearch from "algoliasearch/lite";
-import Autocomplete from "./Autocomplete";
-import { HitSearch, SearchResponse } from "../types";
+
+import { HitSearch } from "../types"; // Adjust the type imports as necessary
 import SuggestionsListComponent from "./Autocomplete";
 
 const indexNameSecret = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME as string;
@@ -11,46 +15,60 @@ const searchClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string,
   process.env.NEXT_PUBLIC_ALGOLIA_API_KEY as string
 );
-
 const index = searchClient.initIndex(indexNameSecret);
 
 const AlgoliaSearchComponent = () => {
   const [showSuggestions, setShowSuggestions] = useState<boolean>(false);
   const [input, setInput] = useState<string>("");
-  const [suggestions, setSuggestions] = useState(
-    ([""] as string[]) || undefined
-  );
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [availableToResearch, setAvailableToResearch] = useState<boolean>(true);
 
   useEffect(() => {
+    if (!availableToResearch) return;
     if (input.length < 3) return;
-    console.warn("CALL TO ALGOLIA");
+
+    console.log("ALOGLI REACHED");
     setShowSuggestions(true);
     index.search(input).then(({ hits }) => {
-      const arraySuggestions: string[] = hits.map((h: Partial<HitSearch>) => {
-        const videoTitle = h.videoTitle ?? "video no encontrado";
-        return videoTitle;
-      });
-      console.log(arraySuggestions);
+      const arraySuggestions: string[] = hits.map(
+        (h: Partial<HitSearch>) => h.videoTitle ?? "video no encontrado"
+      );
       setSuggestions(arraySuggestions);
     });
-  }, [input]);
+  }, [input, availableToResearch]);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const userInput = e.target.value;
-    setInput(userInput);
-    console.log("input", input);
+    setInput(e.target.value);
+    setAvailableToResearch(true);
   };
 
-  const onClick = (e: MouseEvent<HTMLLIElement>) => {
-    console.log("onclick reached");
-    //setSuggestions([""]);
-    //setInput(e.currentTarget.innerText);
-    //setActiveSuggestionIndex(0);
+  const onClick = (index: number, suggestion: string) => {
+    setInput(suggestion);
     setShowSuggestions(false);
+    setAvailableToResearch(false);
+  };
+
+  const newRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  });
+
+  const handleOutsideClick = (e: any) => {
+    console.log("click", e, newRef);
+    if (newRef.current && !newRef.current.contains(e.target)) {
+      console.log("Outside click");
+      if (showSuggestions) setShowSuggestions(false);
+      setAvailableToResearch(true);
+      setInput("");
+    }
   };
 
   return (
-    <div className="relative w-64">
+    <div className="relative w-64" ref={newRef}>
       <input
         className="w-full p-2 border border-gray-300 rounded"
         type="text"
--- src/app/components/Autocomplete.tsx
+++ src/app/components/Autocomplete.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import React, { useState, ChangeEvent, MouseEvent } from "react";
-
 interface SuggestionListProps {
   suggestions: string[];
-  onClick: (event: MouseEvent<HTMLLIElement>) => void;
+  onClick: (index: number, suggestion: string) => void;
 }
 
 const SuggestionsListComponent: React.FC<SuggestionListProps> = ({
@@ -13,13 +12,22 @@ const SuggestionsListComponent: React.FC<SuggestionListProps> = ({
 }) => {
   const [activeSuggestionIndex, setActiveSuggestionIndex] = useState<number>(0);
 
+  const handleItemClick = (index: number, suggestion: string) => {
+    setActiveSuggestionIndex(index); // Set the current active index
+    onClick(index, suggestion); // Call the parent's onClick handler
+  };
+
   return suggestions.length ? (
     <ul className="absolute z-10 left-0 right-0 mt-1 max-h-60 overflow-auto bg-white border border-gray-300 rounded shadow-lg">
       {suggestions.map((suggestion, index) => {
         let className =
           index === activeSuggestionIndex ? "p-2 bg-gray-100" : "p-2";
         return (
-          <li className={className} key={suggestion} onClick={onClick}>
+          <li
+            className={className}
+            key={suggestion}
+            onClick={() => handleItemClick(index, suggestion)}
+          >
             {suggestion}
           </li>
         );
@@ -27,44 +35,8 @@ const SuggestionsListComponent: React.FC<SuggestionListProps> = ({
     </ul>
   ) : (
     <div className="absolute left-0 right-0 mt-1 p-2 bg-white border border-gray-300 rounded shadow-lg">
-      <em>No suggestions, you re on your own!</em>
+      <em>Porfavor, escribe de nuevo</em>
     </div>
   );
 };
-
-/* const Autocomplete: React.FC<AutocompleteProps> = ({ suggestions }) => {
-  const [filteredSuggestions, setFilteredSuggestions] = useState<string[]>([]);
-  const [showSuggestions, setShowSuggestions] = useState<boolean>(false);
-  const [input, setInput] = useState<string>("");
-
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const userInput = e.target.value;
-    setInput(userInput);
-    setFilteredSuggestions(
-      suggestions.filter(
-        (suggestion) =>
-          suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
-      )
-    );
-    setActiveSuggestionIndex(0);
-    setShowSuggestions(true);
-  };
-
-
-
-
-
-  return (
-    <div className="relative w-64">
-      <input
-        type="text"
-        onChange={onChange}
-        value={input}
-        className="w-full p-2 border border-gray-300 rounded"
-      />
-      {showSuggestions && input && <SuggestionsListComponent />}
-    </div>
-  );
-};
- */
 export default SuggestionsListComponent;
