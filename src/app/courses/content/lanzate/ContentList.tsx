49ab467593c6ec949e12a95c16ca96fcfaf4a5c9
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2024-07-19 14:37:46 -0400

    delete unnecessary folders files

--- src/app/courses/content/lanzate/ContentList.tsx
+++
@@ -1,7 +0,0 @@
-import { ContentListType, VideoContent } from "@/app/types";
-import { present24hrsHTML } from "./intro-content";
-
-//! to delete
-export const allVideoUrls: { [key: string]: string } = {
-  "text-estar-presente-por-24-horas": present24hrsHTML,
-};
