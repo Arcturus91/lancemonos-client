dd5a9c98a8f66a4f5ea811c3a08c55bf6fd641d6
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2024-07-29 10:37:48 -0400

    style pdf viewer

--- src/app/courses/components/PdfViewer.tsx
+++ src/app/courses/components/PdfViewer.tsx
@@ -1,20 +1,35 @@
 "use Client";
-import React, { useState, useEffect } from "react";
+import useResizeObserver from "@/app/hooks/useResizeObserver";
+import React, { useState, useEffect, useCallback } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+const resizeObserverOptions = {};
 interface PdfViewerProps {
   pdfUrl: string;
 }
-
+const maxWidth = 1000;
 const PdfViewer: React.FC<PdfViewerProps> = ({ pdfUrl }) => {
   console.log("pdfurl from pdfviewer", pdfUrl);
   const url = pdfUrl;
   const [numPages, setNumPages] = useState<number | null>(null);
   const [pageNumber, setPageNumber] = useState(1);
   const [isLoaded, setIsLoaded] = useState(false);
+
+  const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
+
+  const onResize = useCallback<ResizeObserverCallback>((entries) => {
+    const [entry] = entries;
+
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
+    }
+  }, []);
+
+  useResizeObserver(containerRef, resizeObserverOptions, onResize);
 
   useEffect(() => {
     setIsLoaded(true);
@@ -25,41 +40,43 @@ const PdfViewer: React.FC<PdfViewerProps> = ({ pdfUrl }) => {
   }
 
   return (
-    <div>
+    <div className=".PdfViewer__container">
+      <div className="flex flex-col sm:flex-row items-center justify-between mt-4 w-full max-w-2xl">
+        <p className="text-center text-sm sm:text-base mb-2 sm:mb-0">
+          Page {pageNumber} of {numPages}
+        </p>
+        <div className="flex space-x-2">
+          <button
+            disabled={pageNumber <= 1}
+            onClick={() => setPageNumber(pageNumber - 1)}
+            className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded disabled:bg-gray-400"
+          >
+            Previous
+          </button>
+          <button
+            disabled={numPages !== null && pageNumber >= numPages}
+            onClick={() => setPageNumber(pageNumber + 1)}
+            className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded disabled:bg-gray-400"
+          >
+            Next
+          </button>
+        </div>
+      </div>
       {isLoaded && (
-        <div>
+        <div className="PdfViewer__container__document">
           <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
-            <Page pageNumber={pageNumber} />
+            <Page
+              pageNumber={pageNumber}
+              width={
+                containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth
+              }
+              className="flex justify-center p-4"
+            />
           </Document>
         </div>
       )}
-      <div className="controls">
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
-        <button
-          disabled={pageNumber <= 1}
-          onClick={() => setPageNumber(pageNumber - 1)}
-        >
-          Previous
-        </button>
-        <button
-          disabled={numPages !== null && pageNumber >= numPages}
-          onClick={() => setPageNumber(pageNumber + 1)}
-        >
-          Next
-        </button>
-      </div>
     </div>
   );
 };
 
 export default PdfViewer;
-
-/*
-
-    <div>
-      <h1>hi</h1>
-    </div>
-
-    */
