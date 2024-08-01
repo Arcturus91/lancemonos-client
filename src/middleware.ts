c476d7eac4815dfff5d63feee93412ff85417e54
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2024-08-01 19:04:04 -0400

    add role control to video-upload route

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,14 +11,17 @@ export async function middleware(request: NextRequest) {
   try {
     const secret = new TextEncoder().encode(process.env.JWT_SECRET);
     const verified = await jwtVerify(authTokenValue, secret);
-    console.log("User authenticated:", verified);
+    console.log("User authenticated:", verified, request.url);
+    if (
+      request.url.includes("video-upload") &&
+      verified?.payload?.role === "user"
+    ) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
     return NextResponse.next();
   } catch (error) {
     console.error("Token verification failed:", error);
     return NextResponse.redirect(new URL("/auth", request.url));
   }
 }
-
-export const config = {
-  matcher: ["/courses/:path*", "/video-upload/:path*"],
-};
