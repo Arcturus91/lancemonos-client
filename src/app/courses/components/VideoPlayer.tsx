a4321a5438397d9467d1212e999e17a9a38fd88a
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2024-08-17 00:31:28 -0400

    videoplayer scroll

--- src/app/courses/components/VideoPlayer.tsx
+++ src/app/courses/components/VideoPlayer.tsx
@@ -4,12 +4,12 @@ import React from "react";
 const VideoPlayer: React.FC<any> = ({ videoData }) => {
   const { videoUrl, videoName } = videoData;
   return (
-    <div className="flex flex-col items-center justify-center px-4 py-8 bg-gray-100">
+    <div className="flex flex-col items-center justify-center px-4 py-16 m-4 bg-gray-100 h-full overflow-y-auto">
       {/* Title */}
       <h1 className="mb-4 text-3xl font-bold text-gray-800">{videoName}</h1>
 
       {/* Video */}
-      <div className="relative w-full max-w-2xl mb-6 overflow-hidden bg-white shadow-md aspect-video">
+      <div className="relative w-full max-w-2xl mb-6 bg-white shadow-md aspect-video">
         <video className="w-full h-full" controls muted loop key={videoUrl}>
           <source src={videoUrl} type="video/mp4" />
           Your browser does not support the video tag.
